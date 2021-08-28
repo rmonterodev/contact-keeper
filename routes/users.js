ff71bbf004f7f3ad9fffd33585b17dc856cfc91d
rmonterodev <rmonterodev@gmail.com>
2021-08-28 12:45:44 -0600

    Auth - Token

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-const {check, validationResult} = require('express-validator');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const {check, validationResult} = require('express-validator/check');
 
 const User = require('../models/User');
 
@@ -43,7 +45,17 @@ router.post('/', [
 
         await user.save();
 
-        res.send('User saved');
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+        jwt.sign(payload, config.get('jwtSecret'), {
+            expiresIn: 36000
+        }, (err, token) => {
+            if (err) throw err;
+            res.json({ token });
+        })
 
 ;    } catch (error) {
         console.log(err.message);
