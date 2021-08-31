370ae69462b924375948268a2a82f59092e9e526
Rafael Montero <19507932+rmonterodev@users.noreply.github.com>
2021-08-31 14:16:20 -0600

    Initial UI
    - React Router.
    - Home and About pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import "./App.css";
+import { NavBar } from "./components/layout/NavBar";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Fragment>
+        <NavBar />
+      </Fragment>
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
