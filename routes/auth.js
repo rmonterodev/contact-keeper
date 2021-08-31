d0b51ee71adca78f24ba0d8766da80de0269618b
rmonterodev <rmonterodev@gmail.com>
2021-08-31 09:40:50 -0600

    CRUD Contacts and Client Concurrently

--- routes/auth.js
+++ routes/auth.js
@@ -3,15 +3,23 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
-const {check, validationResult} = require('express-validator/check');
+const auth = require('../middleware/auth');
+const {check, validationResult} = require('express-validator');
 
 const User = require('../models/User');
 
 
 // @route       GET api/auth
 // @desc        Get logged user
-router.get('/', (req, res) => {
-    res.send('Get logged in user');
+// @access      Private
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 // @route       POST api/auth
